9a070c493e3d7acbca3cdc37c1ef87bd85fa2d72
Yato <67519814+Yat-o@users.noreply.github.com>
2020-12-04 13:54:03 -0500

    Update configexample.py

--- configexample.py
+++ configexample.py
@@ -1,7 +1,7 @@
 BOT_PREFIX = ""
 TOKEN = ""
 APPLICATION_ID = ""
-OWNERS = ['']
+OWNERS = [''] #feel free to change to remove the brackets[] if only one owner
 BLACKLIST = []
 STARTUP_COGS = [
     "cogs.general", "cogs.jishaku", "cogs.help", "cogs.moderation", "cogs.owner", "cogs.errrorhandler",
