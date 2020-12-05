db9bd9c12e032525565c5c61fa93dbf01f5fbc40
❥sora <60399731+6days9weeks@users.noreply.github.com>
2020-12-06 01:08:03 +0900

    Create onedit.py

---
+++ cogs/onedit.py
@@ -0,0 +1,50 @@
+import asyncio
+
+import discord
+from discord.ext import commands
+
+
+class OnEdit(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.timeout = 30
+
+    async def edit_process_commands(self, message: discord.Message):
+        """Same as Airi's method (Airi.process_commands), but dont dispatch message_without_command."""
+        if not message.author.bot:
+            ctx = await self.bot.get_context(message)
+            await self.bot.invoke(ctx)
+            if ctx.valid is False:
+                for allowed_name in ("Alias"):
+                    asyncio.ensure_future(listener(message))
+
+    @commands.command()
+    @commands.is_owner()
+    async def edittime(self, ctx, *, timeout: float):
+        """
+        Change how long the bot will listen for message edits to invoke as commands.
+        Defaults to 30 seconds.
+        Set to 0 to disable.
+        """
+        if timeout < 0:
+            timeout = 0
+        await self.config.timeout.set(timeout)
+        self.timeout = timeout
+        await ctx.message.add_reaction("<:checkmark:779670688114016256>")
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        if not after.edited_at:
+            return
+        if before.content == after.content:
+            return
+        if self.timeout is None:
+            self.timeout = await self.config.timeout()
+        if (after.edited_at - after.created_at).total_seconds() > self.timeout:
+            return
+        await self.edit_process_commands(after)
+
+
+def setup(bot):
+    bot.add_cog(OnEdit(bot))
