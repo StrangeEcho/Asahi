173621f0f391c866dd4dd3e736123cc62e1c9a1c
Yat-o <tylergoodmen312@gmail.com>
2020-12-05 15:55:06 +0000

    fix for ala gay

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,161 +8,59 @@ class moderation(commands.Cog, name="moderation"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='kick', pass_context=True)
-    async def kick(self, context, member: discord.Member, *args):
-        if context.message.author.guild_permissions.kick_members:
-            if member.guild_permissions.administrator:
-                embed = discord.Embed(
-                    title="Error!",
-                    description="User has Admin permissions.",
-                    color=0x00FF00
-                )
-                await context.send(embed=embed)
-            else:
-                try:
-                    reason = " ".join(args)
-                    embed = discord.Embed(
-                        title="User Kicked!",
-                        description=f"**{member}** was kicked by **{context.message.author}**!",
-                        color=0x00FF00
-                    )
-                    embed.add_field(
-                        name="Reason:",
-                        value=reason
-                    )
-                    await context.send(embed=embed)
-                    try:
-                        await member.send(
-                            f"You were kicked by **{context.message.author}** from {ctx.guild}!\nReason: {reason}"
-                        )
-                    except:
-                        pass
-                except:
-                    embed = discord.Embed(
-                        title="Error!",
-                        description="An error occurred while trying to kick the user.",
-                        color=0x00FF00
-                    )
-                    await context.message.channel.send(embed=embed)
-        else:
-            embed = discord.Embed(
-                title="Error!",
-                description="You don't have the permission to use this command.",
-                color=0x00FF00
-            )
-            await context.send(embed=embed)
+    @commands.comamnd(brief = 'Kicks a member', aliases = ['k', 'boot'])
+    @commands.has_permissions(kick_members = True)
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        embed = discord.Embed(
+            description=f"Sucessfully Kicked {member.mention} for {reason} from {ctx.guild}",
+            color=0x000000
+        )
+        await ctx.send(embed=embed)
+        await member.send(f'You were kicked from {context.guild} for {reason}.')
 
-    @commands.command(name="nick")
-    async def nick(self, context, member: discord.Member, *, name: str):
-        if context.message.author.guild_permissions.administrator:
-            try:
-                if name.lower() == "!reset":
-                    name = None
-                embed = discord.Embed(
-                    title="Changed Nickname!",
-                    description=f"**{member}'s** new nickname is **{name}**!",
-                    color=0x00FF00
-                )
-                await context.send(embed=embed)
-                await member.change_nickname(name)
-            except:
-                embed = discord.Embed(
-                    title="Error!",
-                    description="An error occurred while trying to change the nickname of the user.",
-                    color=0x00FF00
-                )
-                await context.message.channel.send(embed=embed)
-        else:
-            embed = discord.Embed(
-                title="Error!",
-                description="You don't have the permission to use this command.",
-                color=0x00FF00
-            )
-            await context.send(embed=embed)
+    @commands.command(brief = 'Nickname a member', aliaes = ['nickname'])
+    @comamnds.has_permissions(manage_nicknames = True)
+    async def nick(self, ctx, member : discord.Member, name : str):
+        embed = discord.Embed(
+            description=f"Changed {member.mention}'s Nickname to {name}",
+            color=0x000000
+        )
+        await ctx.send(embed=embed)
+        await member.change_nickname(name)
 
-    @commands.command(name="ban")
-    async def ban(self, context, member: discord.Member, *args):
-        if context.message.author.guild_permissions.administrator:
-            try:
-                if member.guild_permissions.administrator:
-                    embed = discord.Embed(
-                        title="Error!",
-                        description="User has Admin permissions.",
-                        color=0x00FF00
-                    )
-                    await context.send(embed=embed)
-                else:
-                    reason = " ".join(args)
-                    embed = discord.Embed(
-                        title="User Banned!",
-                        description=f"**{member}** was banned by **{context.message.author}**!",
-                        color=0x00FF00
-                    )
-                    embed.add_field(
-                        name="Reason:",
-                        value=reason
-                    )
-                    await context.send(embed=embed)
-                    await member.send(f"You were banned by **{context.message.author}**!\nReason: {reason}")
-                    await member.ban(reason=reason)
-            except:
-                embed = discord.Embed(
-                    title="Error!",
-                    description="An error occurred while trying to ban the user.",
-                    color=0x00FF00
-                )
-                await context.send(embed=embed)
-        else:
-            embed = discord.Embed(
-                title="Error!",
-                description="You don't have the permission to use this command.",
-                color=0x00FF00
-            )
-            await context.send(embed=embed)
+    @commands.command(brief = 'Ban People', aliases = ['b'])
+    @commands.has_permissions(ban_member = True)
+    async def ban(self, ctx, member : discord.Member, *, reason=reason):
+        embed = discord.Member(
+            description=f"Banned {member.mention} for {reason} from {ctx.guild} ",
+            color=0x000000
+        )
+        await ctx.send(embed=embed)
+        await member.send(f'You were banned from {ctx.guild} for {reason}')
 
-    @commands.command(name="warn")
-    async def warn(self, context, member: discord.Member, *args):
-        if context.message.author.guild_permissions.administrator:
-            reason = " ".join(args)
-            embed = discord.Embed(
-                title="User Warned!",
-                description=f"**{member}** was warned by **{context.message.author}**!",
-                color=0x00FF00
-            )
-            embed.add_field(
-                name="Reason:",
-                value=reason
-            )
-            await context.send(embed=embed)
-            try:
-                await member.send(f"You were warned by **{context.message.author}**!\nReason: {reason}")
-            except:
-                pass
-        else:
-            embed = discord.Embed(
-                title="Error!",
-                description="You don't have the permission to use this command.",
-                color=0x00FF00
-            )
-            await context.send(embed=embed)
+    @commands.command()
+    @commands.has_permissions(kick_member = True)
+    async def warn(self, ctx, member : discord.Member, *, reason=None):
+        embed = discord.Embed(
+            description=f"{member.mention} has been warned by {ctx.author.mention}",
+            color=0x000000
+        )
+        embed.add_field(
+            name="Reason:",
+            value=f"{reason}",
+            inline=True
+        )
+        await ctx.send(embed=embed)
 
-    @commands.command(name="purge")
-    async def purge(self, context, number):
-        if context.message.author.guild_permissions.administrator:
-            purged_messages = await context.message.channel.purge(limit=number)
-            embed = discord.Embed(
-                title="Chat Cleared!",
-                description=f"**{context.message.author}** cleared **{len(purged_messages)}** messages!",
-                color=0x00FF00
-            )
-            await context.send(embed=embed)
-        else:
-            embed = discord.Embed(
-                title="Error!",
-                description="You don't have the permission to use this command.",
-                color=0x00FF00
-            )
-            await context.send(embed=embed)
+    @commands.command(brief = 'clears messages', alises = ['prune', 'clear'])
+    async def purge(self, ctx, *, amount = None):
+        embed = discord.Embed(
+            description=f"Cleared {amount} of messages",
+            color=0x000000
+        )    
+        await ctx.channel.purge(limit=amount)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(moderation(bot))
