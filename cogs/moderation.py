fb3ceffe87b3cc3afe3359bc771e78416d734634
Yat-o <tylergoodmen312@gmail.com>
2020-12-10 18:19:27 +0000

    fix autistic mistake

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -30,7 +30,7 @@ class ActionReason(commands.Converter):
         return ret
 
 
-class Moderator(commands.Cog):
+class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = default.config()
