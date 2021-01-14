68a30268dd9808e1a56d9738ab04c2602b853403
Yat-o <tylergoodmen312@gmail.com>
2021-01-14 12:57:34 -0500

    Idk anymore tbh

--- configexample.py
+++ configexample.py
@@ -1,7 +1,7 @@
 BOT_PREFIX = ""
 TOKEN = ""
 APPLICATION_ID = ""
-OWNERS = []  # feel free to change to remove the brackets[] if only one owner
+OWNERS = []  # feel free to change to remove the brackets[] if only one owner. This isnt really needed since @commands.is_owner() exist
 BLACKLIST = []  # Blacklisting is a bit weird. F
 STARTUP_COGS = [
     "cogs.general",
