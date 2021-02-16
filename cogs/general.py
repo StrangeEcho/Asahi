eb1d042a4f017bc0fc585f981605d4e9c8d3c368
Yato <tylergoodmen312@gmail.com>
2021-02-16 17:05:17 -0500

    Rewrite sinfo, fetch commands and organize stuff

--- cogs/general.py
+++ cogs/general.py
@@ -14,32 +14,36 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(aliases=["sinfo"])
     @commands.guild_only()
+    @commands.cooldown(1, 6, commands.BucketType.user)
     async def serverinfo(self, ctx):
-        """Get the guild information."""
-        server = ctx.message.guild
-        roles = [x.name for x in server.roles]
-        role_length = len(roles)
-        if role_length > 50:
-            roles = roles[:50]
-            roles.append(f">>>> Displaying[50/{len(roles)}] Roles")
-        roles = ", ".join(roles)
-        channels = len(server.channels)
-        time = str(server.created_at)
-        time = time.split(" ")
-        time = time[0]
+        embed = discord.Embed(title=ctx.guild.name, color=0xFFB6C1)
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}\nID: {ctx.guild.owner.id}", inline=True)
+        embed.add_field(name="Server ID", value=ctx.guild.id, inline=True)
+        embed.add_field(name="Member Count", value=ctx.guild.member_count, inline=True)
+        embed.add_field(name="Role Count", value=len(ctx.guild.roles), inline=True)
+        embed.add_field(name="Total Voice/Text Channels", value=len(ctx.guild.channels), inline=True)
+        embed.add_field(name="Creation Time", value=ctx.guild.created_at, inline=True)
+        await ctx.send(embed=embed)
 
+    @commands.command()
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def fetch(self, ctx, id : int):
+        user = await self.bot.fetch_user(id)
+        flags = [f.name for f in user.public_flags.all()]
         embed = discord.Embed(
-            title="**Server Name:**", description=f"{server}", color=0xFFB6C1
-        )
-        embed.set_thumbnail(url=server.icon_url)
-        embed.add_field(name="Owner", value=f"{server.owner}\n{server.owner.id}")
-        embed.add_field(name="Server ID", value=server.id)
-        embed.add_field(name="Member Count", value=server.member_count)
-        embed.add_field(name="Text/Voice Channels", value=f"{channels}")
-        embed.add_field(name=f"Roles ({role_length})", value=roles)
-        embed.set_footer(text=f"Created at: {time}")
+            description=(f"""
+            Username: {user}
+            ID: {user.id}
+            Account Creation: {user.created_at}
+            Public Flags: 
+            {flags}
+            """),
+            color=0xFFB6C1
+            )
+        embed.set_thumbnail(url=user.avatar_url)
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -54,27 +58,6 @@ class General(commands.Cog):
         )
         embed.set_footer(text=f"Pong request by {ctx.message.author}")
         await ctx.send(embed=embed)
-
-    @commands.command()
-    @commands.guild_only()
-    async def invite(self, ctx):
-        """Bot invite link."""
-        try:
-            await ctx.author.send(f"Invite me by clicking here: https://discordapp.com/oauth2/authorize?&client_id={config.APPLICATION_ID}&scope=bot&permissions=8")
-            await ctx.send('You Have Mail :envelope:')
-        except discord.Forbidden:
-            await ctx.send(f'I Cannot Direct Message You **{ctx.author.display_name}**\n'
-                            'Go To Your Discord Settings -> Privacy & Safety -> Allow Direct Messages From Sever Members')
-            
-    @commands.command()
-    @commands.guild_only()
-    async def support(self, ctx):
-        """Sends an invite to the bot support server."""
-        try:
-            await ctx.author.send("Join my support server by clicking here: https://discord.gg/GAeb2eXW7a")
-            await ctx.send('You Have Mail :envelope:')
-        except discord.Forbidden:
-            await ctx.send(f'I Cannot Direct Message You **{ctx.author.display_name}**')
 
     @commands.command()
     @commands.guild_only()
@@ -108,7 +91,7 @@ class General(commands.Cog):
                 color=0xFFB6C1,
             )
             await ctx.send(embed=embed)
-
+    #looking to rewrite this soon
     @commands.group()
     async def avatar(self, ctx):
         if ctx.invoked_subcommand is None:
--- cogs/misc.py
+++ cogs/misc.py
@@ -1,5 +1,7 @@
 import discord
 
+import config
+
 from discord.ext import commands
 
 
@@ -14,6 +16,26 @@ class Misc(commands.Cog):
             "See a bug or issue with the bot? Please report it at https://github.com/Yat-o/HimejiBot/issues"
         )
 
+    @commands.command()
+    async def support(self, ctx):
+        """Sends an invite to the bot support server."""
+        try:
+            await ctx.author.send("Join my support server by clicking here: https://discord.gg/GAeb2eXW7a")
+            await ctx.send('You Have Mail :envelope:')
+        except discord.Forbidden:
+            await ctx.send(f'I Cannot Direct Message You **{ctx.author.display_name}**\n'
+                            'Go To Your Discord Settings -> Privacy & Safety -> Allow Direct Messages From Sever Members')
+
+    @commands.command()
+    @commands.guild_only()
+    async def invite(self, ctx):
+        """Bot invite link."""
+        try:
+            await ctx.author.send(f"Invite me by clicking here: https://discordapp.com/oauth2/authorize?&client_id={config.APPLICATION_ID}&scope=bot&permissions=8")
+            await ctx.send('You Have Mail :envelope:')
+        except discord.Forbidden:
+            await ctx.send(f'I Cannot Direct Message You **{ctx.author.display_name}**\n'
+                            'Go To Your Discord Settings -> Privacy & Safety -> Allow Direct Messages From Sever Members')
 
 def setup(bot):
     bot.add_cog(Misc(bot))
