ded344ee88077dceae59f5a5a50a23794e7822bb
6days9weeks <no-reply@w69.wtf>
2021-06-07 15:02:05 +0900

    anothet bug fix

--- cogs/snipe.py
+++ cogs/snipe.py
@@ -18,6 +18,8 @@ edit_snipe = {"author": None, "content": None, "guild": None, "channel": None}
 
 class Snipe(commands.Cog):
     """Some snipe related commands xd"""
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
