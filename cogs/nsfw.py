b4f2f0cd3da4e65c32dda1c2ce6abf15c34f8fdd
Yato <tylergoodmen312@gmail.com>
2021-06-24 16:28:24 -0400

    Add hentai command and move nhentai

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -1,14 +1,14 @@
 import datetime
+from random import choice
 
+import discord
 from discord.ext import commands, menus
 from hentai import Format, Hentai, Tag, Utils
-import discord
 
+from utils.classes import HimejiBot, EmbedListMenu
 from config import OK_COLOR
-from utils.classes import EmbedListMenu, HimejiBot
 
 embed_color = OK_COLOR.replace("#", "0x")
-
 
 class Embed(discord.Embed):
     def __init__(self, colour=int(embed_color, base=16), timestamp=None, **kwargs):
@@ -22,12 +22,57 @@ class Embed(discord.Embed):
         instance.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
         return instance
 
-
-class Nhentai(commands.Cog):
-    """Something for perverts or something idk."""
-
+class NSFW(commands.Cog):
     def __init__(self, bot: HimejiBot):
         self.bot = bot
+
+
+    @commands.command()
+    async def hentai(self, ctx: commands.Context):
+        """Retrieves a hentai gif/image from nekos.life api endpoint"""
+        endpoints = [
+            "Random_hentai_gif",
+            "pussy",
+            "nsfw_neko_gif",
+            "lewd",
+            "les",
+            "kuni",
+            "cum",
+            "classic",
+            "boobs",
+            "bj",
+            "anal",
+            "yuri",
+            "trap",
+            "tits",
+            "solog",
+            "solo",
+            "pwankg",
+            "pussy_jpg",
+            "lewdkemo",
+            "lewdk",
+            "keta",
+            "hololewd",
+            "holoero",
+            "hentai",
+            "femdom",
+            "feetg",
+            "erofeet",
+            "feet",
+            "ero",
+            "erok",
+            "erokemo",
+            "cum_jpg",
+            "gasm"
+        ]
+        async with self.bot.session.get(f"https://nekos.life/api/v2/{choice(endpoints)}") as resp:
+            if resp.status == 200:
+                await ctx.send(embed=discord.Embed(color=self.bot.ok_color).set_image(url=(await resp.json())["url"]))
+            else:
+                await ctx.send(embed=discord.Embed(
+                    description=f"API returned a {resp.status} status. Try again...",
+                    color=self.bot.error_color
+                ))
 
     @commands.group()
     @commands.cooldown(1, 5, commands.BucketType.member)
@@ -90,4 +135,4 @@ class Nhentai(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Nhentai(bot))
+    bot.add_cog(NSFW(bot))
