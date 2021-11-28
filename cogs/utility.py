10b1a885cae83acda9b920ee0927743438884666
Yato <tylergoodmen312@gmail.com>
2021-11-27 21:45:30 -0500

    I forgot this

--- cogs/utility.py
+++ cogs/utility.py
@@ -312,7 +312,7 @@ class Utility(commands.Cog):
         embed.add_field(name="ID", value=user.id)
         embed.add_field(
             name="Status & Activity",
-            value=f"Status: {user.status}\nActivity: {user.activity.name}",
+            value=f"Status: {str(user.status).title()}\nActivity: {user.activity.name}",
         )
         embed.add_field(
             name="Account Creation",
