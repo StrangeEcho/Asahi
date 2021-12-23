d76aa7eb3b440fae142f1372549b15849ffbccbb
Yato <tylergoodmen312@gmail.com>
2021-12-23 18:23:31 -0500

    Fix stuff

--- kurisu/cogs/music.py
+++ kurisu/cogs/music.py
@@ -113,7 +113,7 @@ class Music(commands.Cog):
             for track in results.tracks:
                 player.queue.append(track)
             await ctx.send_ok(f"Added {results.track_count} to the queue")
-            await player.play(results.tracks[player.queue.pop(0)])
+            await player.play(player.queue.pop(0))
             return
 
         if len(results) == 1:
