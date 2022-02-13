cffa21fb102b24efe2ce16154e25d67c21d7ad73
Yato <tylergoodmen312@gmail.com>
2022-02-12 20:25:35 -0500

    Thingie

--- src/cogs/devtools.py
+++ src/cogs/devtools.py
@@ -232,7 +232,7 @@ class DevTools(commands.Cog):
             (await (await asyncio.create_subprocess_shell("git pull", stdout=PIPE)).communicate())[0][:1000],
             "utf-8",
         ).replace("\n", "")
-        await ctx.send_info(pull_output)
+        await ctx.send_info(f"```\n{pull_output}\n```")
         new_version = str(
             (await (await asyncio.create_subprocess_shell("git describe --always", stdout=PIPE)).communicate())[0],
             "utf-8",
