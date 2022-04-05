57fb22210c039a5256044d876d04ee5b205046bc
Yato <tylergoodmen312@gmail.com>
2022-04-05 13:00:13 -0400

    Misc changes

--- src/cogs/meta.py
+++ src/cogs/meta.py
@@ -4,14 +4,19 @@ from core.context import AsahiContext
 from core.bot import Asahi
 from core.database import PrefixHandler
 
-class Meta(commands.Cog):
+class Meta(
+    commands.Cog,
+    command_attrs={
+        "cooldown": commands.CooldownMapping.from_cooldown(1, 3.5, commands.BucketType.user)
+    }
+):
     def __init__(self, bot: Asahi):
         self.bot = bot
-        self.prefix_handler = PrefixHandler(self.bot) 
+        self.prefix_handler = PrefixHandler(self.bot)
     
     @commands.command()
     async def ping(self, ctx: AsahiContext):
-        """Obligitory command"""
+        """Obligitory ping command"""
         msg = await ctx.send("Measuring now...")
         await msg.edit(
             embed=discord.Embed(
@@ -35,6 +40,28 @@ class Meta(commands.Cog):
         
         await self.prefix_handler.add_prefix(prefix, ctx.guild.id)
         await ctx.send_ok(f"Sucessfully changed this guild's prefix to `{prefix}`")
+
+    @commands.command()
+    async def credits(self, ctx: AsahiContext):
+        """Yes..."""
+        await ctx.send(
+            embed=discord.Embed(
+                title="Credits",
+                description=(
+                    "Author: [Yat-o](https://github.com/Yat-o)\n"
+                    "Contributors: A Full list can be found [here](https://github.com/Yat-o/Asahi/graphs/contributors)\n"
+                    f"Registered Bot Owners: {', '.join([str(await self.bot.fetch_user(o)) for o in self.bot.owner_ids])}\n"
+                    "Source Code: Can be found [here](https://github.com/Yat-o/Asahi/) "
+                )
+            ).set_thumbnail(
+                url=self.bot.user.avatar.url
+            )
+        )
+    
+    @commands.command()
+    async def invite(self, ctx: AsahiContext):
+        """Dms you with an invite link to invite the bot with"""
+        await ctx.send_info(f"Invite me using [this link](https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&permissions=413893192823&scope=bot)")
 
 async def setup(bot: Asahi):
     await bot.add_cog(Meta(bot))
